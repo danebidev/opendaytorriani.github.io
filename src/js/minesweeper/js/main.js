befd375d88713e99bdf1724e6d97d6449dd9effc
Ray C <hacker4street@gmail.com>
2023-10-13 19:14:14 +0200

    fixed additional issues

--- src/js/minesweeper/js/main.js
+++ src/js/minesweeper/js/main.js
@@ -101,8 +101,6 @@ function buildTable() {
     </td>
   <tr>
     <td class="menu" id="folder-bar" colspan="${size}">
-      <div id="folder1"><a href="https://github.com/nickarocho/minesweeper/blob/master/readme.md" target="blank">Read Me </a></div>
-      <div id="folder2"><a href="https://github.com/nickarocho/minesweeper" target="blank">Source Code</a></div>
     </td>
   </tr>
   </tr>
